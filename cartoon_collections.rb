ab3302200fa769f862cea4d4d6f779e4c966b8c9
Evan Burton <evan@heyevan.com>
2020-04-17 04:49:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,8 @@ def summon_captain_planet(planeteer_calls)
 planeteer_calls.map {|planeteer| planeteer.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-
+def long_planeteer_calls(planeteer)# code an argument here
+planeteer.all do {|planeteer| planeteer.length > 4} 
 end
 
 def find_the_cheese# code an argument here
