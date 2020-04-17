75af8376e2477050a4c61c20f0d0f728eeda9c1a
Evan Burton <evan@heyevan.com>
2020-04-17 04:31:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,6 @@ def roll_call_dwarves(array)
   array.each_with_index { |dwarf, index| puts "#{index+1}. #{dwarf}" }
 end 
   
-  end
-end
 
 def summon_captain_planet(planeteer_calls)
 planeteer_calls.map! {|planeteer| planeteer.capitalize}
