4264ff1abea39e544ced0aa2af9b05f83badddbc
Evan Burton <evan@heyevan.com>
2020-04-17 05:25:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,5 +18,6 @@ end
 
 def find_the_cheese(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
-cheese_types.include?("cheddar")
+cheese_types.detect do |cheese|
+  cheese = "cheddar"
 end
