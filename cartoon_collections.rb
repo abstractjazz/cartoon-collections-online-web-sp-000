81e7434f7342d00e61c02ab613132c9895d23f04
Evan Burton <evan@heyevan.com>
2020-04-17 04:50:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ planeteer_calls.map {|planeteer| planeteer.capitalize + "!"}
 end
 
 def long_planeteer_calls(planeteer)# code an argument here
-planeteer.all do {|planeteer| planeteer.length > 4} 
+planeteer.all? do {|planeteer| planeteer.length > 4} 
 end
 
 def find_the_cheese# code an argument here
