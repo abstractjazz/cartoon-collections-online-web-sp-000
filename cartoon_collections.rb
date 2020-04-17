3036e9575cae549f64f4f1865ec80c325710165f
Evan Burton <evan@heyevan.com>
2020-04-17 04:33:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,17 @@
 def roll_call_dwarves(array)
   array.each_with_index { |dwarf, index| puts "#{index+1}. #{dwarf}" }
-end 
-  
+end
+
 
 def summon_captain_planet(planeteer_calls)
 planeteer_calls.map! {|planeteer| planeteer.capitalize}
 end
 
+def long_planeteer_calls# code an argument here
 
+end
+
+def find_the_cheese# code an argument here
+
+  cheese_types = ["cheddar", "gouda", "camembert"]
+end
