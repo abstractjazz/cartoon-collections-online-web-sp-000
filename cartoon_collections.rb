5f25ac978c27cbcbe77efaa4f49c6adf103bdc45
Evan Burton <evan@heyevan.com>
2020-04-17 05:46:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,10 +17,8 @@ planeteer.length > 4
 end
 
 def find_the_cheese(array)
-  i = 0
     cheese_types = ["cheddar", "gouda", "camembert"]
-    while i < cheese_types.length
-      return cheese_types[i] if array.include?(cheese_types[i])
-      i += 1
+    array.detect do |cheese|
+    array.include?(cheese_types)
     end
   end
