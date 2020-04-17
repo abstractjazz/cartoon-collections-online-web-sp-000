8db8a09b0f7110228de2a5a51c33c50b9da09304
Evan Burton <evan@heyevan.com>
2020-04-17 04:56:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,8 @@ def long_planeteer_calls(planeteer)# code an argument here
 planeteer.all? do |planeteer|
 planeteer.length > 4
   end
+  planeteer.any? do |planeteer|
+    planeteer.length > 4
 end 
 
 def find_the_cheese# code an argument here
